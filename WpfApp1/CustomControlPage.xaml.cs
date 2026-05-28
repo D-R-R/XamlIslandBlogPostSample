fe34fae21cdddce4277776b91695a54b269c6afb
Daniel R. Regner <Daniel.Regner@tels.net>
2026-05-27 19:49:37 -0500

    Adding a dynamic XAML test.

--- WpfApp1/CustomControlPage.xaml.cs
+++ WpfApp1/CustomControlPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows;
 using Windows.UI.Popups;
 
@@ -9,13 +9,15 @@ namespace WpfApp1
         public CustomControlPage()
         {
             InitializeComponent();
+            uiDynamicUwpXaml.Child = (Windows.UI.Xaml.UIElement)Windows.UI.Xaml.Markup.XamlReader.Load(
+              "<StackPanel xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"><Button>dynamic xaml!</Button><TextBox Text=\"stuff\" /><DatePicker /><TimePicker /></StackPanel>");
         }
 
         private void myUwpButton_ChildChanged(object sender, EventArgs e)
         {
             if (myUwpButton.Child is Windows.UI.Xaml.Controls.Button button)
             {
-                button.Content = "UWP Button";
+                button.Content = "UWP Button from " + System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
                 button.Click += (s, args) =>
                 {
                     MessageBox.Show("UWP Button, WPF MessageBox!");
